8c974a47dae4e4485eb93974b7d76b71bef7ac20
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2020-09-21 10:48:37 +0200

    Modify the install script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,8 +2,33 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+DOTFILES=(".doom.d" ".zshrc")
+
 echo "Dotfiles in '$DOTFILES_DIR'"
 
-echo "Creating symlink to dot.doom.d"
-ln -s "$DOTFILES_DIR/dot.doom.d" ~/.doom.d
+for DOTFILE in "${DOTFILES[@]}"; do
+    echo "Installing symlink for ~/$DOTFILE'"
 
+    if [ -e "$HOME/$DOTFILE" ] ;
+    then
+        if [ -L "$HOME/$DOTFILE" ] ; # Is a symlink
+        then
+            BACKUPEXT="symlink_backup"
+            alias delete="unlink"
+        else
+            BACKUPEXT="backup"
+            alias delete="rm -r"
+        fi
+
+        echo "-> The ~/$DOTFILE exists, creating backup to ~/$DOTFILE.$BACKUPEXT"
+
+        if [ -e  "$HOME/$DOTFILE.$BACKUPEXT" ] ;
+        then
+            delete "$HOME/$DOTFILE.$BACKUPEXT"
+        fi
+
+        cp "$HOME/$DOTFILE" "$HOME/$DOTFILE.$BACKUPEXT" && delete "$HOME/$DOTFILE"
+    fi
+
+    ln -s "$DOTFILES_DIR/dot$DOTFILE" "$HOME/$DOTFILE"
+done
