292775d6f9caee6326a07de61ec5160ea8bb0b14
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2020-09-21 10:51:13 +0200

    Edit install script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -20,15 +20,11 @@ for DOTFILE in "${DOTFILES[@]}"; do
             alias delete="rm -r"
         fi
 
-        echo "-> The ~/$DOTFILE exists, creating backup to ~/$DOTFILE.$BACKUPEXT"
-
-        if [ -e  "$HOME/$DOTFILE.$BACKUPEXT" ] ;
-        then
-            delete "$HOME/$DOTFILE.$BACKUPEXT"
-        fi
-
+        echo "-> The ~/$DOTFILE exists, creating backup to ~/$DOTFILE.$BACKUPEXT"       
         cp "$HOME/$DOTFILE" "$HOME/$DOTFILE.$BACKUPEXT" && delete "$HOME/$DOTFILE"
     fi
 
     ln -s "$DOTFILES_DIR/dot$DOTFILE" "$HOME/$DOTFILE"
 done
+
+echo "Dotfiles installed successfuly!"
