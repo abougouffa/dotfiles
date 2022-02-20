65224bbc758cd4b3547dc3de0f49bb1da93d906e
Abdelhak Bougouffa <abougouffa@cryptolab.net>
2022-02-20 16:25:12 +0100

    emacs: Edit Chemacs2 config

--- .emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default"   . ((user-emacs-directory . "~/.emacs-doom")))
- ("doom"      . ((user-emacs-directory . "~/.emacs-doom")))
- ("scimax"    . ((user-emacs-directory . "~/.emcas-scimax")))
- ("spacemacs" . ((user-emacs-directory . "~/.emacs-spacemacs"))))
