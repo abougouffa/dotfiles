7da809e9956a15f9d5638e2d9eb2186d10f590b1
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-07-27 18:51:16 +0200

    Update PDF export settings

--- dot_doom.d/dot_dir-locals.el
+++ dot_doom.d/dot_dir-locals.el
@@ -1,5 +1,15 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((projectile-project-root . "~/.doom.d/")))
- (nil . ((projectile-project-name . "doom-config"))))
+((org-mode . ((eval . (progn (setq org-latex-listings 'minted)
+                             (setq org-latex-minted-options
+                                   '(("frame" "lines")
+                                     ("fontsize" "\\footnotesize")
+                                     ("tabsize" "2")
+                                     ("breaklines" "")
+                                     ("breakanywhere" "") ;; break anywhere, no just on spaces
+                                     ("style" "default")
+                                     ("bgcolor" "MyBGGray") ;; color defined in the template
+                                     ("linenos" "")))))
+              (geiser-scheme-implementation . 'guile)
+              (org-export-in-background . nil))))
