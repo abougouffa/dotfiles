4e5264260bfbc8a5da8bcdea0293c60bba80ee9a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-09-08 14:18:23 +0200

    refactor: move `guess-language` config to a module

---
+++ dot_doom.d/modules/checkers/guess-language/config.el
@@ -0,0 +1,24 @@
+;;; checkers/guess-language/config.el -*- lexical-binding: t; -*-
+
+(use-package! guess-language
+  :ensure t
+  :hook (text-mode . #'guess-language-mode)
+  :init
+  (setq guess-language-languages '(ar en fr)
+        guess-language-min-paragraph-length 50)
+  :config
+  (defun +set-buffer-language (lang beg end)
+    (let ((buff-lang (pcase lang
+                       ('ar "ar")
+                       ('en "en")
+                       ('fr "fr")
+                       (_   "auto"))))
+      (message "Detected language: %s, setting `lsp-ltex-language' to \"%s\""
+               ;; Get language name from langcodes
+               (catch 'l-name (dolist (l guess-language-langcodes)
+                                (when (eq lang (car l))
+                                  (throw 'l-name (nth 2 l)))))
+               buff-lang)
+      (setq-local lsp-ltex-language buff-lang)))
+
+  (add-hook 'guess-language-after-detection-functions #'+set-buffer-language))
---
+++ dot_doom.d/modules/checkers/guess-language/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; checkers/guess-language/packages.el
+
+(package! guess-language
+  :recipe (:host github
+           :repo "tmalsburg/guess-language.el"))
